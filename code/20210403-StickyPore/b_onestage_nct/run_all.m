51317bf14eb6ab69c1f454a0de6151605f0d48f8
RA <numpde@null.net>
2021-04-07 21:14:58 +0200

    incl sensitivity

--- code/20210403-StickyPore/b_onestage_nct/run_all.m
+++ code/20210403-StickyPore/b_onestage_nct/run_all.m
@@ -28,17 +28,32 @@
 		X.Value = X.Value / v;
 		
 		T = 1e5; % seconds
-		set(getconfigset(m, 'active'), 'Stoptime', T);
+		cs = getconfigset(m, 'active');
+		set(cs, 'Stoptime', T);
 		
-		[t, x, names] = sbiosimulate(m);
+		% https://ch.mathworks.com/help/simbio/ug/example-calculating-sensitivities.html
+		spp = m.Species({m.Species.Name} == "Cargo");
+		cs.SensitivityAnalysisOptions.Outputs = spp;
+		cs.SensitivityAnalysisOptions.Inputs = sbioselect(m, 'Type', 'parameter');
+		cs.SolverOptions.SensitivityAnalysis = true;
+		cs.SensitivityAnalysisOptions.Normalization = 'Full';
+		
+		obj = sbiosimulate(m);
+		t = obj.Time;
+		x = obj.Data;
+		names = obj.DataNames;
+		
+		% T (time), R (sensitivity data for species Ga), S (names of the states specified for sensitivity analysis), and I (names of the input factors used for sensitivity analysis)
+		[T, R, S, I] = getsensmatrix(obj);
+		R = R(end, :, :);
 		
 		name = ['v=' num2str(v)];
 		
 		equations = m.getequations;
-		save(strcat("results", "/", name, ".mat"), 't', 'x', 'names', 'equations', '-nocompression');
+		save(strcat("results", "/", name, ".mat"), 't', 'x', 'names', 'equations', 'T', 'R', 'S', 'I', '-nocompression');
 		
 		disp(strcat("NPC count", " = ", num2str(E.Value), " ", E.Unit, " x ", num2str(X.Value), " ", X.Units))
-		for name = ["Cargo ratio", "Total ImpB ratio", "Total ImpB in envelope"]
+		for name = ["Total cargo ratio", "Free cargo ratio", "Total ImpB ratio", "Total ImpB in envelope"]
 			O = m.Observables({m.Observables.name} == name);
 			disp(strcat(name, " = ", num2str(x(end, names == name)), " ", O.Units))
 		end
